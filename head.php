5379e4b9bc3ab77f6f6584c3d5028fdd86c07ace
佐々木智美 <t.m1ss0.do@gmail.com>
2020-03-16 16:56:00 +0900

    test

---
+++ head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>BooksMap</title>
+    <?php echo Asset::css('dist/css/app.min.css'); ?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" integrity="sha256-UzFD2WYH2U1dQpKDjjZK72VtPeWP50NoJjd26rnAdUI=" crossorigin="anonymous" />
+  </head>
